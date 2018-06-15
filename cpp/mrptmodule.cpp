519618253491d2fc9f138f32bf084d22c10d5d8b
usheth <utsavsheth1994@gmail.com>
2018-06-15 11:06:23 -0400

    Readding filtering by votes

--- cpp/mrptmodule.cpp
+++ cpp/mrptmodule.cpp
@@ -159,6 +159,53 @@ static void mrpt_dealloc(mrptIndex *self) {
     Py_TYPE(self)->tp_free(reinterpret_cast<PyObject *>(self));
 }
 
+
+template<typename T>
+static PyArrayObject* vector_to_nparray(const std::vector<T>& vec, int type_num = PyArray_FLOAT){
+
+   // rows not empty
+   if( !vec.empty() ){
+
+       size_t nRows = vec.size();
+       npy_intp dims[1] = {nRows};
+
+       PyArrayObject* vec_array = (PyArrayObject *) PyArray_SimpleNew(1, dims, type_num);
+       T *vec_array_pointer = (T*) PyArray_DATA(vec_array);
+
+       std::copy(vec.begin(),vec.end(),vec_array_pointer);
+       return vec_array;
+
+   // no data at all
+   } else {
+      npy_intp dims[1] = {0};
+      return (PyArrayObject*) PyArray_ZEROS(1, dims, PyArray_FLOAT, 0);
+   }
+
+}
+
+
+static PyArrayObject *filter_leaves_by_votes(mrptIndex *self, PyObject *args) {
+    PyObject *l;
+
+    int num_leaves,votes_required, dim;
+
+    if (!PyArg_ParseTuple(args, "Oii", &l, &num_leaves, &votes_required))
+        return NULL;
+
+    VectorXi votes = VectorXi::Zero(num_leaves);
+    std::vector<int> voted_leaves;
+    int *leaves = reinterpret_cast<int *>(PyArray_DATA(l));
+    for(int i=0;i<num_leaves;i++,leaves++) {
+        if(++votes(*leaves) == votes_required){
+            voted_leaves.push_back(*leaves);
+        }
+    }
+
+    PyArrayObject *final_leaves = vector_to_nparray(voted_leaves,PyArray_INT);
+    return final_leaves;
+
+}
+
 static PyObject *get_leaf_info(mrptIndex *self, PyObject *args) {
     PyObject *v;
     int len,dimensions;
