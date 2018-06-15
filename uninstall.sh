72cb235a14d42eb13e1d9e38eb6cf227aef0b47b
usheth <utsavsheth1994@gmail.com>
2018-06-15 09:09:48 -0400

    Reverting to original

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-sudo rm -r build
-sudo rm -r mrpt.egg-info
-sudo rm mrptlib.cpython-35m-x86_64-linux-gnu.so
-sudo rm /usr/local/lib/python3.5/dist-packages/easy-install.pth
-sudo rm /usr/local/lib/python3.5/dist-packages/mrpt.egg-link
